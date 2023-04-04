7e2647cb49c802d16a27a88743372ba46f52a281
MeliObre <meliobre06@gmail.com>
2023-04-04 18:17:51 -0300

    comentarios

--- comandos_para_crear_paquete_vortex.R
+++ comandos_para_crear_paquete_vortex.R
@@ -1,32 +1,41 @@
 #  este es un script de trabajo para documentar los pasos a seguir en la creacion
 #  del paquete vortex.
+
+#LIBRERIAS A UTILIZAR:
+
 library("usethis")
 library("devtools")
 library("VariantAnnotation")
 library("ggplot2")
 library("regexcite")
 library("Biostrings")
+
+#Usamos CREATE_PACKAGE para que cree DESCRIPTION
+#Hay alguna manera de generar la descripcion sin utilizar create package??
 create_package("C:/Users/HP/OneDrive/Documentos/GitHub/vortex")
 
-#use_git()#pregunta...
+#use_git() Debemos cargar USE_GIT??
 
 use_r("lector")
 load_all()
 
-#prueba de la funcion lector
+#Prueba de funcionamiento de Lector.runs:
 lector.runs("vortex/datos_crudos2/SraRunInfo_completa_12_12_22.csv",
             folder = "trabajo_6_3_2023",
             output.file = "run",chunklength = 50)
 
-#control 1
+#Control 1
 check()
 
 #Licencia
 use_mit_license()
 
+#Dumentacion
 document()
 
+#Prueba de carga de la funcion
 help("lector.runs")
 
-#control 2
+#Control 2
+#Comienza a fallar...Al momento de controlar la funcion, no la encuentra...
 check()
