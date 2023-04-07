9eaf31ff2a7cb054c9e76ad25ac45b1aef958847
marcocacciabue <64919062+marcocacciabue@users.noreply.github.com>
2023-04-06 22:13:32 -0300

    first steps to develop vortex package. Highly unstable

--- comandos_para_crear_paquete_vortex.R
+++ comandos_para_crear_paquete_vortex.R
@@ -1,20 +1,43 @@
 #  este es un script de trabajo para documentar los pasos a seguir en la creacion
 #  del paquete vortex.
 
-#LIBRERIAS A UTILIZAR:
 
-library("usethis")
-library("devtools")
-library("VariantAnnotation")
-library("ggplot2")
-library("regexcite")
-library("Biostrings")
 
 #Usamos CREATE_PACKAGE para que cree DESCRIPTION
 #Hay alguna manera de generar la descripcion sin utilizar create package??
-create_package("C:/Users/HP/OneDrive/Documentos/GitHub/vortex")
+
+
+### Aca hay que usar path relativos!! Consultar cualquier cosa.
+# create_package("C:/Users/HP/OneDrive/Documentos/GitHub/vortex")
+
+### vamos a una carpeta SUPERIOR a donde este vortex
+devtools::create('./vortex')
+
+### NO suele ser necesario pero por las dudas nos aseguramos que el proyecto activo
+### sea el correcto
+usethis::proj_activate('./vortex')
+
+setwd("./vortex")
+#LIBRERIAS A UTILIZAR:
+### Solo cargamos estas tres librerias para el "desarrollo" del paquete
+### Agrego roxygen2 para la documentacion.
+
+library("usethis")
+library("devtools")
+library("roxygen2")
+
+### estas librerias NO las cargamos las tenemos que agregar a la lista de dependencias
+### usando un comando particular de usethis
+# library("VariantAnnotation")
+# library("ggplot2")
+# library("regexcite")
+# library("Biostrings")
+
 
 #use_git() Debemos cargar USE_GIT??
+### se puede usar git en Rstudio. Sin embargo yo prefiero usar el github desktop
+
+
 
 use_r("lector")
 load_all()
