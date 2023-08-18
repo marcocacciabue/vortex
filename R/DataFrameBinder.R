6e8c69d1d4eb66408964001178e4ddec495e2de5
marcocacciabue <64919062+marcocacciabue@users.noreply.github.com>
2023-08-18 10:51:14 -0300

    read.vcf.to.df now can add sample name

--- R/DataFrameBinder.R
+++ R/DataFrameBinder.R
@@ -1,9 +1,9 @@
 #' DataFrameBinder
 #'
 #' @param vcflist A list of collapsedVCF objects to combine in a single data.frame using
-#' [VCFToDataFrame()]
+#' [VCFToDataFrame]
 #'
-#' @return A [data.frame()]
+#' @return A [data.frame]
 #' @export
 #'
 #' @examples
@@ -26,4 +26,34 @@ DataFrameBinder <- function(vcflist) {
   }
   big_data <- do.call(rbind, listavcf_salida)
   return(big_data)
+}
+
+
+#' read.vcf.to.df
+#'
+#' @param file A string path to the vcf file to read. Created to reemplace [DataFrameBinder].
+#' @param add_sample Logical. if true adds [sample_name] in a new column.
+#' @param sample_name string. Name of sample to add if [add_sample] is TRUE.
+#' @return A [data.frame] created with [VCFToDataFrame].
+#' @export
+#'
+#' @examples
+#' file <- system.file("extdata", "variant_file.vcf", package = "voRtex", mustWork = TRUE)
+#' read.vcf.to.df(file)
+#'
+read.vcf.to.df<-function(file,
+                         add_sample=FALSE,
+                         sample_name){
+  x<-VariantAnnotation::readVcf(file)
+
+  out<-voRtex::VCFToDataFrame(x)
+  if(add_sample & missing(sample_name)){
+    stop("add_sample TRUE and sample_name is missing, please provide a name")
+  }
+  if(add_sample){
+
+
+    out$sample<-sample_name
+  }
+  return(out)
 }
